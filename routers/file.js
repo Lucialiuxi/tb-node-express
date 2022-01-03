4e3b5f9256b1aa716c74a9e691a8dfd1d7972685
Lucialiuxi <1435554190@qq.com>
2022-01-04 06:28:18 +0800

    项目设置完成

--- routers/file.js
+++ routers/file.js
@@ -13,21 +13,6 @@ router.use(bodyParser.urlencoded({ extended: true }))
 //parse application/json
 router.use(bodyParser.json())
 
-/***
- * code:
- *      注册：
- *          0 注册的用户名已经存在
- *          1 注册成功
- *      登录：
- *          404 用户名不存在、密码错误
- *          200  登录成功
- *      新建项目文件夹：
- *          200  新建项目成功
- *      刷新：
- *          3 刷新获取数据成功
- *      
- */
-
 //新建文件夹
 router.post('/createFile',function(req, res){
     const { username, fileName, fileAbstract } = req.body;
@@ -53,22 +38,14 @@ router.post('/createFile',function(req, res){
 
 //进入或者刷新大图标文件区的时候，请求文件数据
 router.post('/findAFileInfoServer',function(req,res,next){
-    console.log("刷新")
     //前端发送过来的用户名存在，就查找用户名对应的数据
     let fileId = req.body.fileId;
     if(fileId){
         FileInfo.find({ fileId },function(err,data){
-            // console.log(err,data)
             if(err){//错误
-                // console.log('err',err)
-                throw new Error(err)
+                commonResponse(res, false, null, undefined, err);
             }else{
-                res.json({
-                    success:true,
-                    code:3,
-                    message:'刷新获取数据成功',
-                    data,
-                })
+                commonResponse(res, true, data, '获取数据成功');
             }
         })
     }
@@ -93,29 +70,17 @@ router.post('/allFilesInfo',function(req, res){
 });
 
 //修改大图标文件的信息 
-router.post('/ModifyFileInfo',function(req,res,next){
-    // console.log('ModifyFileInfo',req.body)
-    // console.log('修改大图标文件')
-    let fileId = req.body.fileId;
-    let username = req.body.username;
+router.post('/modifyFileInfo',function(req, res){
+    let { fileId, username, fileAbstract, fileName } = req.body;
     if(fileId){
-        FileInfo.findOneAndUpdate({
-            fileId: fileId,
-            username: username,
-        },{
-            fileName:req.body.fileName,
-            fileAbstract:req.body.fileAbstract
-        },function(err,data){
+        FileInfo.findOneAndUpdate(
+        { fileId, username },
+        { fileName, fileAbstract},
+        function(err, data){
             if(err){
-               // console.log('err',err) 
-            }
-            if(data.fileId){
-                res.json({
-                    success:true,
-                    code:5,
-                    message:'修改文件信息成功',
-                    afterModifyData:req.body
-                })
+                commonResponse(res, false, null, undefined, err);
+            } else {
+                commonResponse(res, true, data, '修改文件信息成功');
             }
         })
 
@@ -123,81 +88,54 @@ router.post('/ModifyFileInfo',function(req,res,next){
 });
 
 //切换标星
-router.post('/ToggleFileStar',function(req, res){
-    // console.log(req.body);
-    // console.log('切换标星')
-    let fileId = req.body.fileId;
-    let username = req.body.username;
+router.post('/toggleFileStar',function(req, res){
+    let { fileId, username, star } = req.body;
     if(fileId){
         FileInfo.findOneAndUpdate({
-            fileId: fileId,
-            username: username,
+            fileId,
+            username,
         },{
-            star:req.body.star
+            star
         },function(err,data){
             if(err){
-               // console.log('err',err) 
-            }
-            if(data.fileId){
-                res.json({
-                    success:true,
-                    code:5,
-                    message:`切换标星成功${req.body.star}`,
-                    afterModifyData:req.body
-                })
+                commonResponse(res, false, null, undefined, err);
+            } else {
+                commonResponse(res, true, data, `切换标星成功${req.body.star}`);
             }
         })
     }
 });
 
-//移动文件到回收站
-router.post('/MoveFileToRecycleBin',function(req, res){
-    // console.log('移动文件到回收站');
-    let fileId = req.body.fileId;
-    let username = req.body.username;
+// 移动文件到回收站 / 从回收站撤销还原
+router.post('/moveFileToRecycleBin',function(req, res){
+    let { fileId, username, inRecycleBin } = req.body;
     if(fileId){
         FileInfo.findOneAndUpdate({
-            fileId: fileId,
-            username: username,
-        },{
-            inRecycleBin:req.body.inRecycleBin
-        },function(err,data){
+            fileId,
+            username,
+        },{ inRecycleBin },function(err, data){
             if(err){
-               // console.log('err',err) 
-            }
-            if(data.fileId){
-                res.json({
-                    success:true,
-                    code:5,
-                    message:'移动文件到回收站成功',
-                    afterModifyData:req.body
-                })
+                commonResponse(res, false, null, undefined, err);
+            } else {
+                commonResponse(res, true, data, '移动文件到回收站成功');
             }
         })
     }
 });
 
-//删除一个项目文件夹
-router.post('/DeleteAFlie',function(req,res){
-    // console.log('删除回收站的文件夹');
-    let fileId = req.body.fileId;
-    let username = req.body.username;
+// 从回收站删除一个项目文件夹
+router.post('/deleteAFlie',function(req,res){
+    let { fileId, username } = req.body;
     if(fileId){
         FileInfo.findOneAndDelete({
-            fileId: fileId,
-            username: username,
-            inRecycleBin: true
+            fileId,
+            username,
+            inRecycleBin: true,
         },function(err,data){
             if(err){
-               // console.log('err',err) 
-            }
-            if(data.fileId){
-                res.json({
-                    success:true,
-                    code:5,
-                    message:`文件删除成功`,
-                    deletedFileInfo:data
-                })
+                commonResponse(res, false, null, undefined, err);
+            } else {
+                commonResponse(res, true, data, '文件删除成功');
             }
         })
     }
